1d59eb29a58688d8fb4e10e1d21b8f26cb3ed986
David Yu <lewen.yu@gmail.com>
2015-10-25 23:22:47 -0700

    [WIN32] update shell.bat; fix build script

--- win32/build.bat
+++ win32/build.bat
@@ -1,5 +1,5 @@
 @echo off
 @if not exist build mkdir build
 pushd build
-cl -Zi ..\src\win32\win32_dots.cpp ..\src\win32\shims\camera.cpp ..\src\win32\shims\mat.cpp ..\src\win32\shims\frustum.cpp ..\src\win32\rasterizer.cpp user32.lib Gdi32.lib -I..\src\win32\shims
+cl -Zi ..\src\win32_dots.cpp ..\src\shims\camera.cpp ..\src\shims\mat.cpp ..\src\shims\frustum.cpp ..\src\rasterizer.cpp user32.lib Gdi32.lib -I..\src\shims
 popd
--- win32/misc/shell.bat
+++ win32/misc/shell.bat
@@ -1,2 +1,6 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x64
+if not exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x64
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+)
