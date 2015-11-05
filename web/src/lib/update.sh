5a3a7723e5346432bfd43ca3f0d501c6e4f4e068
David Yu <lewen.yu@gmail.com>
2015-11-04 23:53:17 -0800

    [WEB] better vendor update script

--- web/src/lib/update.sh
+++ web/src/lib/update.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
+# usage: download url target
+# where target is the name of the local file target
+download() {
+    local url="$1"
+    local target="$2"
+    [ -f "$target" ] || curl -k "$url" -o "$target"
+}
+
+echo "updating vendor code..."
+
 echo updating libraries...
-curl -O https://raw.githubusercontent.com/vexator/TSM/master/TSM/tsm-0.7.js
-curl -k -O https://raw.githubusercontent.com/ccampbell/mousetrap/master/mousetrap.js
+download https://raw.githubusercontent.com/vexator/TSM/master/TSM/tsm-0.7.js tsm-0.7.js
+download https://raw.githubusercontent.com/ccampbell/mousetrap/master/mousetrap.js mousetrap.js
 
 echo updating typescript definition files...
-curl -O https://raw.githubusercontent.com/vexator/TSM/master/TSM/tsm-0.7.d.ts
-curl -k -O https://raw.githubusercontent.com/borisyankov/DefinitelyTyped/master/mousetrap/mousetrap.d.ts
+download https://raw.githubusercontent.com/vexator/TSM/master/TSM/tsm-0.7.d.ts tsm-0.7.d.ts
+download https://raw.githubusercontent.com/borisyankov/DefinitelyTyped/master/mousetrap/mousetrap.d.ts mousetrap.d.ts
 
 echo all done.
