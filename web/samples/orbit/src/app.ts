62c223784b4d749c7a85915ec4294ab688123654
David Yu <lewen.yu@gmail.com>
2015-11-09 16:18:17 -0800

    [WEB] orbit app doesn't need more than 1 viewport

--- web/samples/orbit/src/app.ts
+++ web/samples/orbit/src/app.ts
@@ -1,11 +1,7 @@
 var shaderRepo: ShaderRepository = null; // global for debugging
 
 interface EditorParams {
-  big : HTMLCanvasElement;
-  tl  : HTMLCanvasElement;
-  tr  : HTMLCanvasElement;
-  bl  : HTMLCanvasElement;
-  br  : HTMLCanvasElement;
+  vp : HTMLCanvasElement;
 }
 
 enum VIEWMODE {
@@ -13,10 +9,6 @@ enum VIEWMODE {
   SPLIT_SMALL_VIEWPORTS,
 }
 
-var frontcam: Camera = null;
-var backcam: Camera = null;
-var sidecam: Camera = null;
-var topcam: Camera = null;
 var pcam: Camera = null;
 
 class Ed {
@@ -26,14 +18,11 @@ class Ed {
 
   constructor( params: EditorParams, shaderRepo: ShaderRepository ) {
     this.viewMode = VIEWMODE.SINGLE_LARGE_VIEWPORT;
-    this.renderer = new Renderer( params.big, shaderRepo );
-    this.camera = frontcam;
+    this.renderer = new Renderer( params.vp, shaderRepo );
+    this.camera = pcam;
     this.renderer.setCamera( this.camera );
 
     setInterval( () => { this.fixedUpdate() }, 1000/30 );
-  }
-
-  buildFourViewports() {
   }
 
   buildSingleViewport() {
@@ -50,18 +39,10 @@ var editor: Ed = null;
 function StartEd() {
   if ( editor == null ) {
 
-    frontcam = new Camera( new TSM.vec3( [ 0, 0, 9 ] ), new TSM.vec3( [ 0, 0, -1 ] ), new TSM.vec3( [ 0, 1, 0 ] ), new TSM.vec3( [ 1, 0, 0 ] ) );
-    backcam = new Camera( new TSM.vec3( [ 0, 0, -9 ] ), new TSM.vec3( [ 0, 0, 1 ] ), new TSM.vec3( [ 0, 1, 0 ] ), new TSM.vec3( [ -1, 0, 0 ] ) );
-    sidecam = new Camera( new TSM.vec3( [ 9, 0, 0 ] ), new TSM.vec3( [ -1, 0, 0 ] ), new TSM.vec3( [ 0, 1, 0 ] ), new TSM.vec3( [ 0, 0, -1 ] ) );
-    topcam = new Camera( new TSM.vec3( [ 0, 9, 0 ] ), new TSM.vec3( [ 0, -1, 0 ] ), new TSM.vec3( [ 0, 0, -1 ] ), new TSM.vec3( [ 1, 0, 0 ] ) );
     pcam = new Camera( new TSM.vec3( [ 3, 5, 9 ] ), new TSM.vec3( [ -3, -5, -9 ] ), new TSM.vec3( [ 0, 1, 0 ] ), new TSM.vec3( [ 1, 0, 0 ] ) );
 
     var params : EditorParams = {
-      big : <HTMLCanvasElement> document.getElementById( "big-viewport" ),
-      tl  : <HTMLCanvasElement> document.getElementById( "small-viewport-tl" ),
-      tr  : <HTMLCanvasElement> document.getElementById( "small-viewport-tr" ),
-      bl  : <HTMLCanvasElement> document.getElementById( "small-viewport-bl" ),
-      br  : <HTMLCanvasElement> document.getElementById( "small-viewport-br" ),
+      vp : <HTMLCanvasElement> document.getElementById( "big-viewport" ),
     };
 
     shaderRepo = new ShaderRepository( ( repo ) => { editor = new Ed( params, repo ); } );
@@ -71,30 +52,5 @@ function StartEd() {
     // testScene.addRenderable( new Cube( 1 ) );
     testScene.addRenderable( new Quad( 1, new DebugMaterial() ) );
     testScene.addLight( new PointLight( new TSM.vec4( [ 9.0, 0, 0, 1.0 ] ), new TSM.vec4( [ 1.0, 0, 0, 1.0 ] ) ) );
-
-    Mousetrap.bind( '1', e => {
-      editor.renderer.setCamera( frontcam );
-      editor.renderer.dirty = true;
-    } );
-
-    Mousetrap.bind( 'alt+1', e => {
-      editor.renderer.setCamera( backcam );
-      editor.renderer.dirty = true;
-    } );
-
-    Mousetrap.bind( '2', e => {
-      editor.renderer.setCamera( sidecam );
-      editor.renderer.dirty = true;
-    } );
-
-    Mousetrap.bind( '3', e => {
-      editor.renderer.setCamera( topcam );
-      editor.renderer.dirty = true;
-    } );
-
-    Mousetrap.bind( 'p', e => {
-      editor.renderer.setCamera( pcam );
-      editor.renderer.dirty = true;
-    } );
   }
 }
