c3c08761f923e13e35da77873883ed4870382cc0
David Yu <lewen.yu@gmail.com>
2015-11-12 00:59:49 -0800

    [WEB] generic matrix

---
+++ web/src/math/mat.ts
@@ -0,0 +1,34 @@
+module gml {
+  /* public-facing matrix (constructor sugar)
+    
+     usage:
+      new Matrix(3,3)(...);
+      new Matrix(4,4)(...);
+      new Matrix(100,6)(...);
+  */
+  export class Mat {
+    constructor( r, c ) {
+      return ( ...values: number[] ) => { return Matrix.apply( Matrix, values.unshift( r, c ) ); }
+    }
+  }
+  
+  // internal matrix implementation; exported because Mat3, Mat4 needs access
+  export class Matrix {
+    values: Float32Array;
+    rows: number;
+    cols: number;
+
+    constructor( rows, cols, ...values: number[] ) {
+      this.rows = rows;
+      this.cols = cols;
+      this.values = new Float32Array( values );
+      if ( this.values.length != this.rows * this.cols ) {
+        console.warn( "input values " + values + " is not " + this.rows * this.cols + " elements long!" );
+      }
+    }
+
+    public get Float32Array(): Float32Array {
+      return this.values;
+    }
+  }
+}
--- web/src/math/mat4.ts
+++ web/src/math/mat4.ts
@@ -1,10 +1,18 @@
 module gml {
   export class Mat4 {
     values: Float32Array;
-    static rows: number = 3;
-    static cols: number = 3;
+    static rows: number = 4;
+    static cols: number = 4;
     constructor( r00, r01, r02, tx, r10, r11, r12, ty, r20, r21, r22, tz, m30, m31, m32, m33 ) {
       this.values = new Float32Array( [ r00, r01, r02, tx, r10, r11, r12, ty, r20, r21, r22, tz, m30, m31, m32, m33 ] );
+    }
+
+    public get( r, c ): number {
+      return this.values[ r * Mat4.cols + c ];
+    }
+
+    public set( r, c, v ) {
+      this.values[ r * Mat4.cols + c ] = v;
     }
   }
 }
