abee47506b729b65e67aa0b11c937508600462c0
David Yu <lewen.yu@gmail.com>
2015-11-12 01:22:35 -0800

    [WEB] simple Mat4 accessors

--- web/src/math/mat4.ts
+++ web/src/math/mat4.ts
@@ -3,5 +3,37 @@ module gml {
     constructor( r00, r01, tx, r10, r11, ty, m20, m21, m22 ) {
       super( 3, r00, r01, tx, r10, r11, ty, m20, m21, m22 );
     }
+
+    public get tx(): number {
+      return this.get( 0, 3 );
+    }
+
+    public get ty(): number {
+      return this.get( 1, 3 );
+    }
+    
+    public get tz(): number {
+      return this.get( 2, 3 );
+    }
+
+    public set tx( v: number ) {
+      this.set( 0, 3, v );
+    }
+
+    public set ty( v: number ) {
+      this.set( 1, 3, v ) ;
+    }
+
+    public set tz( v: number ) {
+      this.set( 2, 3, v ) ;
+    }
+
+    public get w(): number {
+      return this.get( 3, 3 );
+    }
+
+    public set w( v: number ) {
+      this.set( 3, 3, v );
+    }
   }
 }
