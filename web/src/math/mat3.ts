7e9457b637dab225e4958eb1930e299844844323
David Yu <lewen.yu@gmail.com>
2015-11-12 01:13:48 -0800

    [WEB] Mat4 to extend Matrix

--- web/src/math/mat3.ts
+++ web/src/math/mat3.ts
@@ -30,8 +30,6 @@ module gml {
 
     */
 
-    values: Float32Array;
-
     constructor( r00, r01, tx, r10, r11, ty, m20, m21, m22 ) {
       super( 3, r00, r01, tx, r10, r11, ty, m20, m21, m22 );
     }
--- web/src/math/mat4.ts
+++ web/src/math/mat4.ts
@@ -1,18 +1,7 @@
 module gml {
   export class Mat4 {
-    values: Float32Array;
-    static rows: number = 4;
-    static cols: number = 4;
-    constructor( r00, r01, r02, tx, r10, r11, r12, ty, r20, r21, r22, tz, m30, m31, m32, m33 ) {
-      this.values = new Float32Array( [ r00, r01, r02, tx, r10, r11, r12, ty, r20, r21, r22, tz, m30, m31, m32, m33 ] );
-    }
-
-    public get( r, c ): number {
-      return this.values[ r * Mat4.cols + c ];
-    }
-
-    public set( r, c, v ) {
-      this.values[ r * Mat4.cols + c ] = v;
+    constructor( r00, r01, tx, r10, r11, ty, m20, m21, m22 ) {
+      super( 3, r00, r01, tx, r10, r11, ty, m20, m21, m22 );
     }
   }
 }
