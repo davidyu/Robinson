6f7845b7fbf36bb952f4b8ea3950fe3d1b38e933
David Yu <lewen.yu@gmail.com>
2015-11-13 17:09:46 -0800

    [WEB] create Mat4 from rows

--- web/src/math/mat4.ts
+++ web/src/math/mat4.ts
@@ -54,6 +54,13 @@ module gml {
 
   }
 
+  export function makeMat4FromRows( r1, r2, r3, r4 ) {
+    return new Mat4( r1.x, r1.y, r1.z, r1.w
+                   , r2.x, r2.y, r2.z, r2.w
+                   , r3.x, r3.y, r3.z, r3.w
+                   , r4.x, r4.y, r4.z, r4.w );
+  }
+
   export function makePerspective( fov: Angle, aspectRatio: number, near: number, far: number ): Mat4 {
     let t = near * Math.tan( fov.toRadians() );
     let r = t * aspectRatio;
@@ -69,7 +76,9 @@ module gml {
   }
 
   export function makeLookAt( pos: Vec4, aim: Vec4 /* target */, up: Vec4, right: Vec4 ): Mat4 {
+    var x = right.normalized;
+    var y = up.normalized;
     var z = aim.sub( pos ).normalized;
-    return new Mat4( 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 );
+    return makeMat4FromRows( x, y, z, new Vec4( 0, 0, 0, 1 ) );
   }
 }
