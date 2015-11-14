2330771d664fc98daaf84296be9f9e16f30b5ed9
David Yu <lewen.yu@gmail.com>
2015-11-13 17:25:54 -0800

    [WEB] camera to use our own matrix and vector

--- web/src/camera.ts
+++ web/src/camera.ts
@@ -1,29 +1,29 @@
 class Camera {
   // note: this matrix is stored in a column major layout, because we are targeting WebGL.
   // TSM.mat4.lookAt() creates the matrix in the layout we expect, so use that.
-  public matrix: TSM.mat4;
+  public matrix: gml.Mat4;
 
-  constructor( position: TSM.vec3, aim: TSM.vec3, up: TSM.vec3, right: TSM.vec3 ) {
-    this.matrix = TSM.mat4.lookAt( position, TSM.vec3.sum( position, aim ), up );
+  constructor( position: gml.Vec4, aim: gml.Vec4, up: gml.Vec4, right: gml.Vec4 ) {
+    this.matrix = gml.makeLookAt( position, aim, up, right );
   }
 
-  public get pos(): TSM.vec3 {
-    return new TSM.vec3( this.matrix.row( 3 ) );
+  public get pos(): gml.Vec4 {
+    return this.matrix.row( 3 );
   }
 
-  public set pos( val: TSM.vec3 ) {
-    this.matrix = TSM.mat4.lookAt( val, TSM.vec3.sum( val, this.aim ), this.up );
+  public set pos( val: gml.Vec4 ) {
+    // this.matrix = TSM.mat4.lookAt( val, gml.Vec4.sum( val, this.aim ), this.up );
   }
 
-  public get aim(): TSM.vec3 {
-    return new TSM.vec3( this.matrix.row( 2 ) );
+  public get aim(): gml.Vec4 {
+    return this.matrix.row( 2 );
   }
 
-  public get up(): TSM.vec3 {
-    return new TSM.vec3( this.matrix.row( 1 ) );
+  public get up(): gml.Vec4 {
+    return this.matrix.row( 1 );
   }
 
-  public get right(): TSM.vec3 {
-    return new TSM.vec3( this.matrix.row( 0 ) );
+  public get right(): gml.Vec4 {
+    return this.matrix.row( 0 );
   }
 }
