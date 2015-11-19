1c0be3919088ddca2f53fe945922c2e2a4428ecc
David Yu <lewen.yu@gmail.com>
2015-11-18 21:09:44 -0800

    [WEB] Mat3 convenience constructors

--- web/src/math/mat3.ts
+++ web/src/math/mat3.ts
@@ -30,8 +30,12 @@ module gml {
 
     */
 
-    constructor( r00, r01, tx, r10, r11, ty, m20, m21, m22 ) {
-      super( 3, 3, r00, r01, tx, r10, r11, ty, m20, m21, m22 );
+    constructor( args: Float32Array );
+    constructor( args: number[] );
+    constructor( r00, r01, tx, r10, r11, ty, m20, m21, m22 );
+
+    constructor( ...args: any[] ) {
+      super( 3, 3, args );
     }
 
     public get tx(): number {
