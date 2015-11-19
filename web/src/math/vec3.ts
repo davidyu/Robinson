5c64da081ecfb5076011e12c6a81b7a539fe8259
David Yu <lewen.yu@gmail.com>
2015-11-18 21:14:01 -0800

    [WEB] vec3 accessors

--- web/src/math/vec3.ts
+++ web/src/math/vec3.ts
@@ -3,5 +3,17 @@ module gml {
     constructor( x, y, z ) {
       super( 3, x, y, z );
     }
+
+    public get x(): number {
+      return this.get( 0 );
+    }
+
+    public get y(): number {
+      return this.get( 1 );
+    }
+
+    public get z(): number {
+      return this.get( 2 );
+    }
   }
 }
