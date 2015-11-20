65539cd66c0467f57ab747c3fb64acd5dbef099b
David Yu <lewen.yu@gmail.com>
2015-11-20 15:46:55 -0800

    [WEB] LU decomposition test

--- web/test/unit/math.js
+++ web/test/unit/math.js
@@ -159,4 +159,65 @@ describe( "mat4 tests", function() {
 
     expect( c.mul( d ) ).toEqual( e );
   } );
+
+  it( "tests LU decomposition", function() {
+    var a = gml.Mat4.identity();
+
+    var row0 = new gml.Vec4( 1, 2, 3, 4 );
+    var row1 = new gml.Vec4( 5, 6, 7, 8 );
+    var row2 = new gml.Vec4( 9,10,11,12 );
+    var row3 = new gml.Vec4( 13,14,15,16 );
+
+    a.setRow( 0, row0 );
+    a.setRow( 1, row1 );
+    a.setRow( 2, row2 );
+    a.setRow( 3, row3 );
+
+    expect( a.row( 0 ) ).toEqual( row0 );
+    expect( a.row( 1 ) ).toEqual( row1 );
+    expect( a.row( 2 ) ).toEqual( row2 );
+    expect( a.row( 3 ) ).toEqual( row3 );
+
+    var col0 = new gml.Vec4( 10, 50, 90,130 );
+    var col1 = new gml.Vec4( 20, 60,100,140 );
+    var col2 = new gml.Vec4( 30, 70,110,150 );
+    var col3 = new gml.Vec4( 40, 80,120,160 );
+
+    a.setColumn( 0, col0 );
+    a.setColumn( 1, col1 );
+    a.setColumn( 2, col2 );
+    a.setColumn( 3, col3 );
+
+    expect( a.column( 0 ) ).toEqual( col0 );
+    expect( a.column( 1 ) ).toEqual( col1 );
+    expect( a.column( 2 ) ).toEqual( col2 );
+    expect( a.column( 3 ) ).toEqual( col3 );
+  } );
+
+  it( "tests mat4 matrix multiplication", function() {
+    var a = new gml.Mat4( 1, 5, 9,13
+                        , 2, 6,10,14
+                        , 3, 7,11,15
+                        , 4, 8,12,16 );
+
+    var lu = a.lu();
+    var l = lu.l;
+    var u = lu.u;
+
+    // make sure l is a lower triangular matrix
+    for ( var i = 0; i < l.rows; i++ ) {
+      expect( l.get( i, i ) ).toBe( 1 );
+      for ( var j = i + 1; j < l.cols; j++ ) {
+        expect( l.get( i, j ) ).toBe( 0 );
+      }
+    }
+
+    // make sure u is a upper triangular matrix
+    for ( var i = 0; i < l.rows; i++ ) {
+      expect( l.get( i, i ) ).toBe( 1 );
+      for ( var j = 0; j < i; j++ ) {
+        expect( l.get( i, j ) ).toBe( 0 );
+      }
+    }
+  } );
 } );
