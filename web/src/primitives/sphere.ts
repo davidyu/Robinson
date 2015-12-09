bfaff6d9536a4a7b5e137c02090a525532f21575
David Yu <lewen.yu@gmail.com>
2015-12-08 21:13:22 -0800

    [WEB] basic UV sphere mesh

--- web/src/primitives/sphere.ts
+++ web/src/primitives/sphere.ts
@@ -1,4 +1,9 @@
 ///<reference path='prim.ts' />
+
+//
+// sphere.ts
+// UV sphere (most basic sphere mesh)
+
 class Sphere extends Primitive implements Renderable {
   material: Material;
   renderData: RenderData;
@@ -13,5 +18,36 @@ class Sphere extends Primitive implements Renderable {
   }
 
   public rebuildRenderData() {
+    let vertices = [];
+    let indices = [];
+    const parallels = 20;
+    const meridians = 40;
+    if ( this.renderData.dirty ) {
+      for ( let j = 0; j < parallels; j++ ) {
+        let parallel = Math.PI * ( j + 1 ) / parallels;
+        for ( let i = 0; i <= meridians; i++) {
+          let meridian = 2 * Math.PI * i / meridians;
+          let x = Math.sin( meridian ) * Math.cos( parallel );
+          let y = Math.sin( meridian ) * Math.sin( parallel );
+          let z = Math.cos( meridian );
+          vertices.push( x );
+          vertices.push( y );
+          vertices.push( z );
+        }
+      }
+
+      for ( let j = 0; j < parallels - 1; j++ ) {
+        for ( let i = 0; i < meridians; i++) {
+          indices.push( j * meridians + i );
+          indices.push( j * meridians + ( i + 1 ) % meridians );
+          indices.push( ( j + 1 ) * meridians + ( i + 1 ) % meridians );
+        }
+      }
+
+      this.renderData.vertices = new Float32Array( vertices );
+      this.renderData.normals = new Float32Array( vertices ); // for a sphere located at 0,0,0, the normals are exactly the same as the vertices
+      this.renderData.indices = new Float32Array( indices );
+      this.renderData.dirty = false;
+    }
   }
 }
