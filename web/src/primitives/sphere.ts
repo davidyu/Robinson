f15207751a098f2372e1c9bdfcdff790f97ec610
David Yu <lewen.yu@gmail.com>
2015-12-24 15:04:09 -0800

    [WEB] parameterize parallels and meridians

--- web/src/primitives/sphere.ts
+++ web/src/primitives/sphere.ts
@@ -7,26 +7,28 @@
 class Sphere extends Primitive implements Renderable {
   material: Material;
   renderData: RenderData;
+  parallels: number;
+  meridians: number;
 
-  constructor( size: number = 1, mat: Material = new BlinnPhongMaterial() ) {
+  constructor( size: number = 1, mat: Material = new BlinnPhongMaterial(), parallels: number = 15, meridians: number = 30 ) {
     super();
     this.transform.scale = new gml.Vec3( size, size, size );
     this.material = mat;
     this.renderData = new RenderData();
     // trigger a rebuild when the renderer updates
     this.renderData.dirty = true;
+    this.parallels = parallels;
+    this.meridians = meridians;
   }
 
   public rebuildRenderData() {
     let vertices = [];
     let indices = [];
-    const parallels = 10;
-    const meridians = 20;
     if ( this.renderData.dirty ) {
-      for ( let j = 0; j < parallels; j++ ) {
-        let parallel = Math.PI * j / ( parallels - 1 );
-        for ( let i = 0; i < meridians; i++) {
-          let meridian = 2 * Math.PI * i / ( meridians - 1 );
+      for ( let j = 0; j < this.parallels; j++ ) {
+        let parallel = Math.PI * j / ( this.parallels - 1 );
+        for ( let i = 0; i < this.meridians; i++) {
+          let meridian = 2 * Math.PI * i / ( this.meridians - 1 );
           let x = Math.sin( meridian ) * Math.cos( parallel );
           let y = Math.sin( meridian ) * Math.sin( parallel );
           let z = Math.cos( meridian );
@@ -36,17 +38,17 @@ class Sphere extends Primitive implements Renderable {
         }
       }
 
-      for ( let j = 0; j < parallels - 1; j++ ) {
-        for ( let i = 0; i < meridians; i++) {
-          let nextj = ( j + 1 ); // loop invariant: j + 1 < parallels
-          let nexti = ( i + 1 ) % meridians;
-          indices.push( j * meridians + i );
-          indices.push( j * meridians + nexti );
-          indices.push( nextj * meridians + nexti );
+      for ( let j = 0; j < this.parallels - 1; j++ ) {
+        for ( let i = 0; i < this.meridians; i++) {
+          let nextj = ( j + 1 ); // loop invariant: j + 1 < this.parallels
+          let nexti = ( i + 1 ) % this.meridians;
+          indices.push( j * this.meridians + i );
+          indices.push( j * this.meridians + nexti );
+          indices.push( nextj * this.meridians + nexti );
 
-          indices.push( j * meridians + i );
-          indices.push( nextj * meridians + nexti );
-          indices.push( nextj * meridians + i );
+          indices.push( j * this.meridians + i );
+          indices.push( nextj * this.meridians + nexti );
+          indices.push( nextj * this.meridians + i );
         }
       }
 
