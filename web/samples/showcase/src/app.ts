38e46b22c778376c994ceef0c87a348f40c7005b
David Yu <lewen.yu@gmail.com>
2015-12-28 21:28:41 -0800

    [WEB] flip things in the sample to match the skybox

--- web/samples/showcase/src/app.ts
+++ web/samples/showcase/src/app.ts
@@ -23,7 +23,7 @@ class ShowcaseApp {
     this.renderer = new Renderer( params.vp, shaderRepo, new gml.Vec4( 0.8, 0.8, 0.8, 1 ) );
     this.orbitCenter = params.orbitCenter;
     this.orbitDistance = params.orbitDistance;
-    this.yaw = gml.fromDegrees( 0 );
+    this.yaw = gml.fromDegrees( 180 );
     this.pitch = gml.fromDegrees( 0 );
     this.renderer.setCamera( this.camera );
     this.dirty = true;
@@ -100,14 +100,14 @@ function StartApp() {
     Scene.setActiveScene( testScene );
 
     // sphere 1, Lambert
-    testScene.addRenderable( new Sphere( 1, new gml.Vec4( -4, 0, 0, 1 ), new LambertMaterial( new gml.Vec4( 0.5, 0.5, 0.5, 1 ) ) ) );
+    testScene.addRenderable( new Sphere( 1, new gml.Vec4( 4, 0, 0, 1 ), new LambertMaterial( new gml.Vec4( 0.5, 0.5, 0.5, 1 ) ) ) );
 
     // sphere 1, Oren-Nayar with some roughness
-    testScene.addRenderable( new Sphere( 1, new gml.Vec4( -1.33333, 0, 0, 1 ), new OrenNayarMaterial( new gml.Vec4( 0.5, 0.5, 0.5, 1 ), 0.3 ) ) );
+    testScene.addRenderable( new Sphere( 1, new gml.Vec4( 1.33333, 0, 0, 1 ), new OrenNayarMaterial( new gml.Vec4( 0.5, 0.5, 0.5, 1 ), 0.3 ) ) );
 
     // sphere 2, good old Blinn-Phong
     testScene.addRenderable( new Sphere( 1
-                                       , new gml.Vec4( 1.33333, 0, 0, 1 )
+                                       , new gml.Vec4( -1.33333, 0, 0, 1 )
                                        , new BlinnPhongMaterial( new gml.Vec4( 0, 0, 0, 1 )
                                                                , new gml.Vec4( 0.5, 0.5, 0.5, 1 )
                                                                , new gml.Vec4( 0.5, 0.5, 0.5, 1 )
@@ -115,12 +115,12 @@ function StartApp() {
                                                                , 16 ) ) );
     // sphere 2, Cook-Torrance
     testScene.addRenderable( new Sphere( 1
-                                       , new gml.Vec4( 4, 0, 0, 1 )
+                                       , new gml.Vec4( -4, 0, 0, 1 )
                                        , new CookTorranceMaterial( new gml.Vec4( 0.5, 0.5, 0.5, 1 )
                                                                  , new gml.Vec4( 0.5, 0.5, 0.5, 1 )
                                                                  , 0.7
                                                                  , 1.586 ) ) );
 
-    testScene.addLight( new PointLight( new gml.Vec4( 0, 5, 10, 1 ), new gml.Vec4( 1.0, 1.0, 1.0, 1.0 ), 10 ) );
+    testScene.addLight( new PointLight( new gml.Vec4( -12, 5, -10, 1 ), new gml.Vec4( 1.0, 1.0, 1.0, 1.0 ), 100 ) );
   }
 }
