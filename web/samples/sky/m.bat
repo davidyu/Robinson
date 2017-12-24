ec1f28f05977a48ea2e5a073a511d92d8493823f
David Yu <lewen.yu@gmail.com>
2017-12-24 10:39:11 -0800

    copy shaders and app page when building sky sample

--- web/samples/sky/m.bat
+++ web/samples/sky/m.bat
@@ -4,6 +4,9 @@
 @call dir /b /s "..\..\src\*.ts" | findstr /v /i ".*\dist\.* .*\2d\.*" > files.txt
 @call dir /b /s "*.ts" >> files.txt
 
-@tsc @files.txt --target ES5 --sourceMap --out %DIST%\app.js
+@call tsc @files.txt --target ES5 --sourceMap --out %DIST%\app.js
 
-@rm files.txt
+@call rm files.txt
+
+xcopy /iy ..\..\dist\shaders %DIST%\shaders\ > NUL
+xcopy /y app.html %DIST%\ > NUL
