49f52fe3ea2eba6996276a1d57b811a553375996
David Yu <lewen.yu@gmail.com>
2018-05-12 12:41:20 -0700

    move windows port to a different repo

--- win32/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-@if not exist build mkdir build
-pushd build
-cl -Zi ..\src\win32_dots.cpp ..\src\shims\camera.cpp ..\src\shims\mat.cpp ..\src\shims\frustum.cpp ..\src\rasterizer.cpp user32.lib Gdi32.lib -I..\src\shims
-popd
