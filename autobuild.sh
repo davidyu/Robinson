acc788dbc2112341c9be43491f7960467ee4f684
David Yu <lewen.yu@gmail.com>
2018-12-30 09:03:00 -0800

    ignore samples folder in autobuild

--- autobuild.sh
+++ autobuild.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v fswatch)" ]; then
 fi
 
 # use fswatch to run the make command any time any file except src.txt changes in this directory
-fswatch -o . -e "dist" -e "src.txt" -e ".git" | while read; \
+fswatch -o . -e "samples" -e "dist" -e "src.txt" -e ".git" | while read; \
     do \
         make autobuild || true
     done
