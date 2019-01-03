c31832d128dfab1a2d6d91d47922cd06fa2d4d68
David Yu <lewen.yu@gmail.com>
2019-01-02 19:57:51 -0800

    convert skybox shader setup to repoV2

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -297,6 +297,19 @@ class Renderer {
         shader = lib.compileProgram( gl, "basic.vert", "cook-torrance-legacy.frag", "cook-torrance" );
       }
       shader = lib.compileProgram( gl, "skybox.vert", "skybox.frag", "skybox" );
+      if ( shader != null ) {
+        shader.attributes[ "aVertexPosition" ] = gl.getAttribLocation( shader.program, "aVertexPosition" );
+        shader.uniforms[ "environment" ] = gl.getUniformLocation( shader.program, "environment" )
+        shader.uniforms[ "uInverseProjectionMatrix" ] = gl.getUniformLocation( shader.program, "uInverseProjectionMatrix" );
+        shader.uniforms[ "uInverseViewMatrix" ] = gl.getUniformLocation( shader.program, "uInverseViewMatrix" );
+        shader.setup = ( gl, attributes, uniforms ) => {
+          gl.disableVertexAttribArray( 0 );
+          gl.disableVertexAttribArray( 1 );
+          gl.disableVertexAttribArray( 2 );
+          gl.enableVertexAttribArray( attributes.aVertexPosition );
+        };
+      }
+
       shader = lib.compileProgram( gl, "skybox.vert", "sky.frag", "sky" );
       shader = lib.compileProgram( gl, "water.vert", "water.frag", "water" );
       if ( shader != null ) {
@@ -722,8 +735,11 @@ class Renderer {
       perspective = gml.makePerspective( gml.fromDegrees( 45 ), viewportW / viewportH, 0.1, 1000.0 );
     }
 
+    let shader = this.repoV2.programs.skybox;
     if ( scene.environmentMap != null ) {
-      this.useProgram( gl, SHADER_PROGRAM.SKYBOX );
+      gl.useProgram( shader.program );
+      shader.setup( gl, shader.attributes, shader.uniforms );
+      // this.useProgram( gl, SHADER_PROGRAM.SKYBOX );
     } else {
       this.useProgram( gl, SHADER_PROGRAM.SKY ); // this shader program automatically moves our quad near the far clip plane, so we don't need to transform it ourselves here
     }
@@ -731,12 +747,12 @@ class Renderer {
     let shaderVariables = this.programData[ this.currentProgram ].uniforms;
 
     let inverseProjectionMatrix = perspective.invert();
-    gl.uniformMatrix4fv( shaderVariables.uInverseProjection, false, inverseProjectionMatrix.m );
+    gl.uniformMatrix4fv( shaderVariables.uInverseProjection, false, inverseProjectionMatrix.m ); // TODO remove me
+    gl.uniformMatrix4fv( shader.uniforms.uInverseProjectionMatrix, false, inverseProjectionMatrix.m );
 
     let inverseViewMatrix = mvStack[ mvStack.length - 1 ].invert().mat3;
-    gl.uniformMatrix3fv( shaderVariables.uInverseView, false, inverseViewMatrix.m );
-
-    gl.uniformMatrix4fv( shaderVariables.uModelToWorld, false, this.fullscreenQuad.transform.m );
+    gl.uniformMatrix3fv( shaderVariables.uInverseView, false, inverseViewMatrix.m ); // TODO remove me
+    gl.uniformMatrix3fv( shader.uniforms.uInverseViewMatrix, false, inverseViewMatrix.m );
 
     if ( this.camera != null ) {
       gl.uniform4fv( shaderVariables.uCameraPos, this.camera.matrix.translation.negate().v );
@@ -746,11 +762,13 @@ class Renderer {
 
     gl.bindBuffer( gl.ARRAY_BUFFER, this.fullscreenQuad.renderData.vertexBuffer );
     gl.vertexAttribPointer( shaderVariables.aVertexPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.vertexAttribPointer( shader.attributes.aVertexPosition, 3, gl.FLOAT, false, 0, 0 );
 
     gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.fullscreenQuad.renderData.indexBuffer );
 
-    if ( this.currentProgram == SHADER_PROGRAM.SKYBOX ) {
-      gl.uniform1i( shaderVariables.uEnvMap, 0 );
+    if ( scene.environmentMap != null ) {
+      let shader = this.repoV2.programs.skybox;
+      gl.uniform1i( shader.uniforms.environment, 0 );
       gl.activeTexture( gl.TEXTURE0 );
       gl.bindTexture( gl.TEXTURE_CUBE_MAP, scene.environmentMap.cubeMapTexture );
     }
